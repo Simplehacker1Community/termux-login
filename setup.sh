fed77fa2abd5a9a16259b959b916ad6cf8f32ef9
Simplehacker1Community <93964893+Simplehacker1Community@users.noreply.github.com>
2022-03-12 07:20:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 apt update
 apt upgrade -y
-apt install python fish curl wget zip -y
+apt install python fish espeak  curl wget zip -y
 pip install termuxlogin
 
 echo installed
 rm termuxlogin.whl
 rm setup.sh
+espeak -v Annie -s 1 -p 140 Termux Login Installed
 bash login
 
